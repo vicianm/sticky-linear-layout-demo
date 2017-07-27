7a2cb33b23f30e7b1a42d94bcb3504b3c20cefb8
Michal Vician <vicianm@gmail.com>
2017-07-27 13:28:30 +0200

    onCreateViewHeader/Footer callbacks

--- src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
+++ src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
@@ -2,6 +2,7 @@ package sk.plaut.dynamicformheader.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called from layout XML file to request
+     * <code>View</code> instance which will be used
+     * as a form header.
+     */
+    public View createHeaderView(View formView) {
+        return null;
+    }
+
+    /**
+     * Called from layout XML file to request
+     * <code>View</code> instance which will be used
+     * as a form footer.
+     */
+    public View createFooterView(View formView) {
+        return null;
     }
 
 }
