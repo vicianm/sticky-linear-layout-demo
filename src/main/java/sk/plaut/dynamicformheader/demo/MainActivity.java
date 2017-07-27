ebd6f28fad03f51399c32d91bd46024555d892b2
loch <lochlukas@gmail.com>
2017-07-27 13:26:28 +0200

    call  on header / footer callback with pinAllowed view as param

--- src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
+++ src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
@@ -2,6 +2,9 @@ package sk.plaut.dynamicformheader.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public View onCreateHeaderTest(View v){
+        Button button = new Button(getApplicationContext());
+        if(v instanceof Button) {
+            button.setText(((Button) v).getText());
+        }
+        return button;
+    }
+
+    public View onCreateFooterTest(View v){
+        Button button = new Button(getApplicationContext());
+        if(v instanceof Button) {
+            button.setText(((Button) v).getText());
+        }
+        return button;
     }
 
 }
