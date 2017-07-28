346e7cd5c3937a0e727cb2e5cbbf4b086a17e74d
Michal Vician <vicianm@gmail.com>
2017-07-28 18:14:12 +0200

    Background transition between active/inactive sections

--- src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
+++ src/main/java/sk/plaut/dynamicformheader/demo/MainActivity.java
@@ -1,16 +1,24 @@
 package sk.plaut.dynamicformheader.demo;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
+import sk.plaut.dynamicformheader.DynamicFormHeaderLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DynamicFormHeaderLayout rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootView = (DynamicFormHeaderLayout)findViewById(R.id.activity_main);
     }
 
     /**
@@ -35,6 +43,33 @@ public class MainActivity extends AppCompatActivity {
         TextView footerTextView = (TextView)getLayoutInflater().inflate(R.layout.section_title, null);
         footerTextView.setText(formTextView.getText());
         return footerTextView;
+    }
+
+    /**
+     * Called from layout XML file to notify that new form section is active.
+     * @param headers List of all section headers of the form (including the active one).
+     * @param activeHeaderIndex Index of the active section header in provided <code>formSectionHeaders</code> list.
+     * @param previousHeaderIndex Index of the section header which was active in a previous call of this method.
+     */
+    public void activeSectionChanged(List<View> headers, int activeHeaderIndex, int previousHeaderIndex) {
+
+        for (int i = 0; i<headers.size(); i++) {
+            TextView header = (TextView)headers.get(i);
+            int childLayoutIndex = rootView.getFormLayout().indexOfChild(header);
+            View sectionContent = rootView.getFormLayout().getChildAt(childLayoutIndex + 1);
+
+            TransitionDrawable headerTransition = (TransitionDrawable)header.getBackground();
+            TransitionDrawable contentTransition = (TransitionDrawable)sectionContent.getBackground();
+
+            if (i == activeHeaderIndex) {
+                headerTransition.startTransition(200);
+                contentTransition.startTransition(200);
+            } else if (i == previousHeaderIndex) {
+                headerTransition.reverseTransition(200);
+                contentTransition.reverseTransition(200);
+            }
+        }
+
     }
 
 }
